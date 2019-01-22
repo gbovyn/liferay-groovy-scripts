a4a7e283fe018654c02183b278df865ef9395f25
Gatien Bovyn <32069404+gbovyn@users.noreply.github.com>
2019-01-22 09:36:10 +0100

    Add log statements
    Helps to keep track of background task

--- replace-in-articles.groovy
+++ replace-in-articles.groovy
@@ -1,10 +1,18 @@
 import com.liferay.portal.kernel.dao.orm.QueryUtil
 import com.liferay.journal.service.JournalArticleLocalServiceUtil
 import com.liferay.portal.kernel.util.PortalUtil
+import org.slf4j.LoggerFactory
 
-final PATTERN = '/group/guest'
-final LOCALES = ['nl_BE', 'fr_FR']
-final GROUP_ID = 10180
+LOGGER = LoggerFactory.getLogger("${this.getClass().getName()} - Replace in articles")
+
+previewMode = true
+
+PATTERN = '/group/guest'
+NEW_VALUE = '/guest'
+LOCALES = ['nl_BE', 'fr_FR']
+GROUP_ID = 10180
+
+CONTEXT_SIZE = 20
 
 List articles = JournalArticleLocalServiceUtil.getArticles(GROUP_ID)
 
@@ -16,11 +24,29 @@ LOCALES.each { locale ->
 		.filter {
 			it.content.with { contains(locale) && contains(PATTERN) }
 		}.each {
-			println("Updating article ${it.article.getArticleId()} (${locale})")
+			trace("Updating article ${it.article.getArticleId()} (${locale})")
+			trace("Before: <pre><xmp>${onlyContext(it.content)}</xmp></pre>")
+			trace("After:  <pre><xmp>${onlyContext(it.content).replace(PATTERN, NEW_VALUE)}</xmp></pre>")
 
-			JournalArticleLocalServiceUtil.updateContent(
-				it.article.getGroupId(), it.article.getArticleId(), 
-				it.article.getVersion(), it.content.replace(PATTERN, '/')
-			)
+			if (!previewMode) {
+				JournalArticleLocalServiceUtil.updateContent(
+					it.article.getGroupId(), it.article.getArticleId(),
+					it.article.getVersion(), it.content.replace(PATTERN, NEW_VALUE)
+				)
+			}
 		}
+}
+
+def onlyContext(String html) {
+	def position = html.indexOf(PATTERN)
+	html.substring((position > CONTEXT_SIZE) ? (position - CONTEXT_SIZE) : 0, (position + PATTERN.length() + CONTEXT_SIZE))
+}
+
+def trace(String message) {
+	println(message)
+	LOGGER.info(removeXMLTags(message))
+}
+
+def removeXMLTags(String str) {
+	str.minus("<pre><xmp>").minus("</xmp></pre>")
 }
